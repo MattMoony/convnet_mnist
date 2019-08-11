fixed j/epoch-plot function; updated loss function; finished gradient-computation function; added sgd; etc.

main.py
@@ -48,7 +48,7 @@ def plot_j_epoch(past_J):
     plt.figure()
     plt.title('J/Epoch-Graph')
 
-    plt.plot(np.arange(pastJ.shape[0]), pastJ, c='darkslategray', linestyle='-')
+    plt.plot(np.arange(len(past_J)), past_J, c='darkslategray', linestyle='-')
 
     plt.xlabel('Epoch')
     plt.ylabel('Cost (J)')
@@ -96,10 +96,33 @@ def normalize(mu, sigma, *args):
 def convolve(x, W):
     fshape = tuple(np.subtract(x.shape[1:-1], W.shape[1:-1]) + 1)
     sm = np.lib.stride_tricks.as_strided(x, (x.shape[0], *fshape, *W.shape[1:-1], x.shape[-1]), tuple(x.strides[:-1]) + tuple(x.strides[1:]))
-    res = np.sum(sm * W[0], (5,4,3))
+    res = np.sum(sm * W[0], (5,4,3))[:,:,:,np.newaxis]
     for w in W[1:]:
-        res = np.hstack((res, np.sum(sm * w, (5,4,3))))
-    return res.reshape(x.shape[0], *fshape, W.shape[0])
+        res = np.concatenate((res, np.sum(sm * w, (5,4,3))[:,:,:,np.newaxis]), axis=3)
+    return res
+
+def deconvolve(a, g, W):
+    fshape = tuple(np.subtract(a.shape[1:-1], W.shape[1:-1]) + 1)
+    sm = np.lib.stride_tricks.as_strided(a, (a.shape[0], *fshape, *W.shape[1:-1], a.shape[-1]), tuple(a.strides[:-1]) + tuple(a.strides[1:]))
+    # print(a.shape)
+    # print(g.shape)
+    # print(W.shape)
+    # print(sm.shape)
+    # print('-'*24)
+    res = np.sum(sm * np.repeat(g[:,:,:,0], np.prod(W.shape[1:]), axis=2).reshape(sm.shape), axis=(2,1,0))
+    for i in range(1, g.shape[-1]):
+        res = np.vstack((res, np.sum(sm * np.repeat(g[:,:,:,i], np.prod(W.shape[1:]), axis=2).reshape(sm.shape), axis=(2,1,0))))
+    return res.reshape(W.shape)
+
+def de_con_actv(o, w):
+    u = np.repeat(o, np.prod(w.shape[1:]), axis=3).reshape(*o.shape, *w.shape[1:]) * w.reshape(1,1,1,*w.shape)
+    u = np.sum(u, axis=3)
+    oshape = (o.shape[0], *np.add(np.subtract(o.shape[1:-1], 1), w.shape[1:-1]), w.shape[-1])
+    res = np.zeros(oshape)
+    for y in range(o.shape[1]):
+        for x in range(o.shape[2]):
+            res[:,y:y+w.shape[1],x:x+w.shape[2],:] += u[:,y,x,:,:,:]
+    return res
 
 def ReLU(z):
     return np.maximum(0, z)
@@ -143,17 +166,17 @@ def predict(x, conv_ws, fc_ws, bs):
 def predict_s(x, conv_ws, fc_ws, bs):
     return predict(x[np.newaxis,:], conv_ws, fc_ws, bs)
 
-def loss(X, y, conv_ws, fc_ws, bs):
+def loss(X, y, conv_ws, fc_ws, bs, lamb=1e-3):
     preds = predict(X, conv_ws, fc_ws, bs)
     y = y.copy().squeeze()
 
     l = np.sum(-np.log(preds[np.arange(X.shape[0]), y]))
     l /= X.shape[0]
 
-    l += (1 / (2 * X.shape[0])) * np.sum(np.asarray([np.sum(np.asarray(x[0]) ** 2) for x in conv_ws + fc_ws]))
+    l += (lamb / (2 * X.shape[0])) * np.sum(np.asarray([np.sum(np.asarray(x[0]) ** 2) for x in conv_ws + fc_ws]))
     return l
 
-def compute_gradient(X, y, conv_ws, fc_ws, bs):
+def compute_gradient(X, y, conv_ws, fc_ws, bs, lamb=1e-3):
     # -- FORWARD PASS ------------------------------------------------------------------------- #
 
     zl = 0
@@ -195,14 +218,15 @@ def compute_gradient(X, y, conv_ws, fc_ws, bs):
     al_grad = np.ones((al_s[-1].shape))
     wi -= 1
 
+    m = X.shape[0]
+
     for w, actf, actf_g in reversed(fc_ws):
         ac_grad = al_grad * actf_g(zl_s[wi], y.squeeze())
 
-        bs_grads.append(np.sum(ac_grad))
-        fc_grads.append(al_s[wi].T.dot(ac_grad))
+        bs_grads.append(np.sum(ac_grad) / m)
+        fc_grads.append(al_s[wi].T.dot(ac_grad) / m + lamb * (w / m))
 
         al_grad = ac_grad.dot(w.T)
-
         wi -= 1
 
     al_grad = al_grad.reshape(last_conv_shape)
@@ -210,8 +234,11 @@ def compute_gradient(X, y, conv_ws, fc_ws, bs):
     for w, actf, actf_g in reversed(conv_ws):
         ac_grad = al_grad * actf_g(zl_s[wi], y.squeeze())
 
-        bs_grads.append(np.sum(ac_grad, axis=(0,1,2)))
-        conv_grads.append()
+        bs_grads.append(np.sum(ac_grad, axis=(0,1,2)) / m)
+        conv_grads.append(deconvolve(al_s[wi], ac_grad, w) / m + lamb * (w / m))
+
+        al_grad = de_con_actv(ac_grad, w)
+        wi -= 1
 
     # -- RETURN VALUES ------------------------------------------------------------------------ #
 
@@ -222,6 +249,40 @@ def compute_gradient(X, y, conv_ws, fc_ws, bs):
     return conv_grads, fc_grads, bs_grads
 
     # -- FIN ---------------------------------------------------------------------------------- #
+
+def sgd(X, y, conv_ws, fc_ws, bs, lamb=0.001, iters=10, alpha=1, batch_size=16):
+    conv_ws = conv_ws.copy()
+    for i in range(len(conv_ws)):
+        conv_ws[i][0] = conv_ws[i][0].copy()
+    fc_ws = fc_ws.copy()
+    for i in range(len(fc_ws)):
+        fc_ws[i][0] = fc_ws[i][0].copy()
+    bs = bs.copy()
+    for i in range(len(bs)):
+        bs[i] = bs[i].copy()
+
+    a_inds = np.arange(X.shape[0])
+    inds = np.random.choice(a_inds, batch_size)
+
+    past_Js = [loss(X[inds], y[inds], conv_ws, fc_ws, bs, lamb)]
+
+    for i in range(iters):
+        inds = np.random.choice(a_inds, batch_size)
+        conv_g, fc_g, bs_g = compute_gradient(X[inds], y[inds], conv_ws, fc_ws, bs, lamb)
+
+        for j in range(len(conv_ws)):
+            conv_ws[j][0] -= alpha * conv_g[j]
+        for j in range(len(fc_ws)):
+            fc_ws[j][0] -= alpha * fc_g[j]
+        for j in range(len(bs)):
+            bs[j] -= alpha * bs[j]
+
+        inds = np.random.choice(a_inds, batch_size)
+        past_Js.append(loss(X[inds], y[inds], conv_ws, fc_ws, bs, lamb))
+
+        print('[iter#{:04d}] Loss: {:f} ... '.format(i, past_Js[-1]))
+
+    return [conv_ws, fc_ws, bs, past_Js]
 
 # -------------------------------------------------------------------------------------------------------------------------------- #
 # -------------------------------------------------------------------------------------------------------------------------------- #
@@ -275,25 +336,25 @@ def main():
     b2 = np.zeros(2) + 1e-3
     b3 = np.zeros(1) + 1e-3
 
-    conv_ws = [(W1, ReLU, ReLU_grad), (W2, ReLU, ReLU_grad)]
-    fc_ws = [(W3, softmax, softmax_grad)]
+    conv_ws = [[W1, ReLU, ReLU_grad], [W2, ReLU, ReLU_grad]]
+    fc_ws = [[W3, softmax, softmax_grad]]
     bs = [b1, b2, b3]
 
     # -- MODEL TRAINING ----------------------------------------------------------------------- #
 
-    conv_ws_grad, fc_ws_grad, bs_grad = compute_gradient(x_train[:10], y_train[:10], conv_ws, fc_ws, bs)
-
-    print(conv_ws_grad)
-    print(fc_ws_grad)
-    print(bs_grad)
+    n_conv_ws, n_fc_ws, n_bs, past_Js = sgd(x_train, y_train, conv_ws, fc_ws, bs, 
+        lamb=0.5, iters=1024, alpha=1, batch_size=32)
     
     # -- MODEL EVALUATION --------------------------------------------------------------------- #
     
-    print(loss(x_train[:10], y_train[:10], conv_ws, fc_ws, bs))
+
 
     # -- MODEL VISUALIZATION ------------------------------------------------------------------ #
 
-    # show_conv_weights([W1, W2])
+    plt.ioff()
+    plot_j_epoch(past_Js)
+
+    plt.show()
 
     # -- FIN ---------------------------------------------------------------------------------- #
 